10e0daabecac180d6845f2d37e6a555299f5c3ce
sjanapatiForce <sjanapati@murraystate.edu>
2024-09-28 21:46:41 -0500

    Second commit adding items and removing items

--- functions.py
+++ functions.py
@@ -47,3 +47,37 @@ def process_customer_request(menu, request_code, quantity):
                 return False
     print(f"Item {request_code} not found on the menu.")
     return False
+
+# Update an existing menu item (either price or name)
+def update_menu_item(menu, code, field, new_value):
+    for item in menu:
+        if item['code'] == code:
+            if field == 'name':
+                item['name'] = new_value
+            elif field == 'price':
+                item['price'] = int(new_value)
+            print(f"{code} updated: {field} changed to {new_value}")
+            return True
+    print(f"Item {code} not found.")
+    return False
+
+# Add a new menu item
+def add_menu_item(menu, code, name, price, stock):
+    new_item = {"code": code, "name": name, "price": int(price), "stock": int(stock)}
+    menu.append(new_item)
+    print(f"Added new item: {new_item}")
+
+# Removing an item from the menu
+def remove_menu_item(menu, code):
+    for item in menu:
+        if item['code'] == code:
+            menu.remove(item)
+            print(f"Removed item: {code}")
+            return True
+    print(f"Item {code} not found.")
+    return False
+
+#display the menu items
+def display_menu(menu):
+    for item in menu:
+        print(f"{item['code']} - {item['name']} - ${item['price']} (stock : {item['stock']})")
